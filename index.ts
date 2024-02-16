d44662ea4dd5f11efa6179a12b2258bd13a807c6
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-02-16 17:01:31 +0100

    initialized the project

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { dataSource } from "./src/db";
+import { httpServer } from "./src/http_server";
+import { webSocketServerRun } from "./src/websocket";
+
+const HTTP_PORT = 8181;
+
+dataSource.initialize().then(() => {
+  webSocketServerRun();
+  httpServer.listen(HTTP_PORT);
+  console.log(`Start static http server on the ${HTTP_PORT} port!`);
+});
